93156869f136aeacac629a95a9b8a6949d5fe161
Gilbert West <gilbertwest@gmail.com>
2017-03-16 15:41:32 +0100

    new minutes template

---
+++ source/_layouts/minutes.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>@yield('title')</title>
+        <meta name="description" content="The Laravel Brussel Meetup is a group of PHP developers enthusiastic about
+        working with and sharing knowledge about the Laravel framework">
+        <link rel="dns-prefetch" href="//maxcdn.bootstrapcdn.com">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+
+        @include('_nav')
+
+        <div class="row align-middle">
+            <div class="minutes columns small-10">
+            @yield('body')
+            </div>
+        </div>
+
+    </body>
+    <!-- we haz no JS -->
+</html>
